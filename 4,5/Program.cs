0ffc801c5a7960a3fcfbcfae3a4436d7f29ca7f8
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-12-23 20:47:42 +0100

    Add project files.

---
+++ 4,5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jämna tal mellan 1 och 20:");
+
+           
+            for (int i = 1; i <= 20; i++)
+            {
+                
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
